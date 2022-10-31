4220189134191dcf36603c41484a2ab86c883564
emmanuel <adebowaleemmanuel331@gmail.com>
2022-10-31 15:23:20 -0700

    0x07. C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,10 +3,10 @@
 /**
  * _strpbrk - function that searches a string for any of a set of bytes
  * @s:first occurrence in the string
- * accept: matches one of the bytes, or @NULL if no such byte
+ * @accept: matches one of the bytes, or @NULL if no such byte
  * Return: a pointer to the byte
  */
-char *_strpbrk(char *s, char * @accept)
+char *_strpbrk(char *s, char *accept)
 {
 	unsigned int i, j;
 
